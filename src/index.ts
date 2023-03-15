63aefabeb44af2879974bb9c3c4272799b09f75f
Kasparas-G55 <kasparas.galdikas55@gmail.com>
2023-03-15 23:37:50 +0200

    chore: initial index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export function testFunction (name: string) {
-  return `Hello ${name}`
+export default class Nodesera {
+  /**
+   * Paysera's API version.
+   */
+  public static readonly VERSION = '1.6';
+
+  /**
+   * Server URL where all requests should go.
+   */
+  public static readonly PAY_URL = 'https://bank.paysera.com/pay';
 }
