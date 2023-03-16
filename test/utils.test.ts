84db319e563cefe483b7b618c63af53453a3e9a6
Kasparas-G55 <kasparas.galdikas55@gmail.com>
2023-03-16 22:09:06 +0200

    test: `createURLFromRequest` should return url with data and sign queries.

---
+++ test/utils.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest';
+import { createRequestBuilder } from '../src/requestbuilder';
+import { createURLFromRequest } from '../src/utils';
+
+describe('utils', () => {
+  it('should return a paysera url with data and sign queries.', () => {
+    const requestBuilder = createRequestBuilder(process.env.VITE_PROJECT_PASSWORD ?? 'test');
+    const query = requestBuilder.buildRequest({
+      projectid: 'test',
+      accepturl: 'test',
+      callbackurl: 'test',
+      cancelurl: 'test',
+      orderid: 'test',
+      version: 'test'
+    });
+
+    expect(createURLFromRequest(query))
+      .toBe('https://bank.paysera.com/pay?data=cHJvamVjdGlkPXRlc3QmYWNjZXB0dXJsPXRlc3QmY2FsbGJhY2t1cmw9dGVzdCZjYW5jZWx1cmw9dGVzdCZvcmRlcmlkPXRlc3QmdmVyc2lvbj10ZXN0&sign=623c93b53648ef2bdd1d5f0af4e7554b');
+  });
+});
