1b522f94f13e1abc4cdb85388946b142ea5bc1e8
Kasparas-G55 <kasparas.galdikas55@gmail.com>
2024-03-14 22:41:08 +0200

    simplify code by merging `requestBuilder.ts` in to `createNodesera` function

--- src/nodesera.ts
+++ src/nodesera.ts
@@ -1,8 +1,8 @@
 import { NodeseraException } from './errors';
-import { createRequestBuilder } from './requestbuilder';
-import { RequestParams } from './types';
-import { verifySignMD5Hash, verifySignRSAKey } from './validation';
-import { createURLFromRequest, decodeURLBase64, decodeURLBase64ToString } from './utils';
+import { QueryParameters, RequestParams } from './types';
+import { requestValidation, verifySignMD5Hash, verifySignRSAKey } from './validation';
+import { createURLFromRequest, decodeURLBase64, decodeURLBase64ToString, encodeURLBase64 } from './utils';
+import { createHash } from 'node:crypto';
 
 /**
  * Paysera's Latest API version.
@@ -14,19 +14,41 @@ export const VERSION = '1.6';
  */
 export const PAY_URL = 'https://bank.paysera.com/pay';
 
-export function createNodesera() {
+export function createNodesera(request: RequestParams) {
 
-  function buildRequest(request: RequestParams) {
-    if (!request.projectid || !request.projectPassword) {
-      throw new NodeseraException();
-    }
-
-    const requestBuilder = createRequestBuilder(request);
-
-    return requestBuilder.buildRequest();
+  if (!request.projectid) {
+    throw new NodeseraException('projectid is required.');
   }
 
-  return { VERSION, PAY_URL, buildRequest };
+  if (!request.projectPassword) {
+    throw new NodeseraException('projectPassword is required.');
+  }
+
+  request.version ??= VERSION;
+
+  return {
+    get data() {
+      return request;
+    },
+
+    set data(newRequestParams: RequestParams) {
+      this.data = newRequestParams;
+    },
+
+    /**
+     * This method encodes your request parameters, creates a md5 hashed sign and returns both in an object.
+     *
+     * @returns {QueryParameters} Query Parameters
+     */
+    createRequest(): QueryParameters {
+      requestValidation(this.data);
+      const base64EncodedURL = encodeURLBase64(this.data);
+      return {
+        data: base64EncodedURL,
+        sign: createHash('md5').update(base64EncodedURL + this.data.projectPassword).digest('hex')
+      };
+    }
+  };
 }
 
 export { verifySignMD5Hash, verifySignRSAKey, createURLFromRequest, decodeURLBase64, decodeURLBase64ToString };
